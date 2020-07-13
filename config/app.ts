0feda1328693d9dd15e8540a1256e92f318641be
André Lins <andrelucas01@hotmail.com>
2020-07-13 20:10:41 -0300

    First Commit

---
+++ config/app.ts
@@ -0,0 +1,9 @@
+import { AppConfig } from 'recife';
+
+export const config: AppConfig = {
+  appName: 'recife-mongoose',
+  basePath: 'src',
+  port: 8100,
+  host: 'localhost',
+  httpFramework: 'express'
+};
